fc887f489b9d2d23e7e8320775843f1a51e0f899
hazen <sam.dep000@rentonstudent.us>
2021-09-27 16:48:28 -0700

    Added Lift Subsystem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Lift.java
@@ -0,0 +1,90 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+public class Lift implements Subsystem {
+
+	DcMotorEx rightMotor;
+	DcMotorEx leftMotor;
+
+	double wheelRadius;
+
+	AngleUnit angleUnit;
+
+	final double PULSES_PER_REVOLUTION = 537.7;
+	final double GEAR_RATIO = 19.2;
+
+	public Lift( HardwareMap hw ) {
+		this( hw, 0.5, AngleUnit.DEGREES );
+	}
+
+	public Lift( HardwareMap hw, double spoolRadius, AngleUnit angleUnit ) {
+		setup( hw );
+		setWheelRadius( spoolRadius );
+		setAngleUnit( angleUnit );
+	}
+
+	public void setup( HardwareMap hw ) {
+
+		leftMotor = hardwareMap.get( DcMotorEx.class, "leftMotor" );
+		rightMotor = hardwareMap.get( DcMotorEx.class, "rightMotor" );
+
+		rightMotor.setDirection( DcMotorSimple.Direction.REVERSE );
+	}
+
+	public void setVelocity( double velocity ) {
+
+		leftMotor.setVelocity( velocity, angleUnit );
+		rightMotor.setVelocity( velocity, angleUnit );
+	}
+
+	public void setPosition( int position ) {
+		leftMotor.setTargetPosition( position );
+		rightMotor.setTargetPosition( position );
+	}
+
+	public void setLiftPosition( double velocity, double position ) {
+		setPosition( convertDistTicks( position, 2 * wheelRadius * Math.PI ) );
+		setVelocity( velocity );
+		while( leftMotor.isBusy() || rightMotor.isBusy() );
+		setVelocity( 0 );
+	}
+
+	public int convertDistTicks( double distanceToTravel, double circumference ) {
+		double revolutions = distanceToTravel / circumference;
+		int totalTicks = (int) Math.round( (revolutions * PULSES_PER_REVOLUTION) / GEAR_RATIO );
+
+		return totalTicks;
+	}
+
+	public int convertTicksDist( double ticksToTravel, double circumference ) {
+		double calculations = ticksToTravel * circumference * GEAR_RATIO;
+		int totalDistance = (int) Math.round( calculations / PULSES_PER_REVOLUTION );
+
+		return totalDistance;
+	}
+
+	// setters and getters for angleUnit
+	public void setAngleUnit( AngleUnit angleUnit ) {
+		this.angleUnit = angleUnit;
+	}
+
+	public AngleUnit getAngleUnit( ) {
+		return angleUnit;
+	}
+
+	// setters and getters for spoolRadius
+	public void setWheelRadius( double newRadius ) {
+		wheelRadius = newRadius;
+	}
+
+	public double getWheelRadius( ) {
+		return wheelRadius;
+	}
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/LiftTest.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.teamcode.subsystems.Lift;
+
+public class LiftTest extends LinearOpMode {
+
+	Lift lift;
+
+	@Override
+	public void runOpMode( ) {
+
+		lift = new Lift( hardwareMap );
+
+		telemetry.addLine( "init finished" );
+		telemetry.update();
+
+		waitForStart();
+
+	}
+}
