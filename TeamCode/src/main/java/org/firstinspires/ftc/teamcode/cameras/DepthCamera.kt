5034fe16370e97b9a15864ce198dcea5434c1d29
Sam DePoule <sam.dep000@rentonstudent.us>
2021-10-07 16:55:08 -0700

    Finished setting up camera

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cameras/DepthCamera.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cameras/DepthCamera.kt
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.cameras
 
 import com.qualcomm.robotcore.hardware.HardwareDevice
 import org.intel.rs.Context
+import org.intel.rs.frame.DepthFrame
 import org.intel.rs.pipeline.Config
 import org.intel.rs.pipeline.Pipeline
 import org.intel.rs.types.Format
@@ -10,13 +11,13 @@ import org.intel.rs.types.Stream
 
 class DepthCamera {
 
-    lateinit var depth: Pose
+    lateinit var depth: DepthFrame
     private val context = Context()
     private val pipeline = Pipeline(context)
 
     init {
         val config = Config()
-        config.enableStream(Stream.Depth, Format.Xyz32f)
+        config.enableStream(Stream.Depth, Format.Z16)
 
         pipeline.start(config)
     }
