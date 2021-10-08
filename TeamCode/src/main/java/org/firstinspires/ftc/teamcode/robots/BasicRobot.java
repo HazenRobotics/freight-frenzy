bc1dea244a93a7125db24267ec0fc904635f058a
Sam DePoule <sam.dep000@rentonstudent.us>
2021-10-08 15:21:33 -0700

    Edited BasicRobot.java.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/BasicRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robots/BasicRobot.java
@@ -1,19 +1,14 @@
 package org.firstinspires.ftc.teamcode.robots;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.drives.MecanumDrive;
 import org.firstinspires.ftc.teamcode.subsystems.Bucket;
 import org.firstinspires.ftc.teamcode.subsystems.CarouselSpinner;
 import org.firstinspires.ftc.teamcode.subsystems.Intake;
 import org.firstinspires.ftc.teamcode.subsystems.Lift;
-import org.firstinspires.ftc.teamcode.utils.TensorFlowUtil;
 
 public class BasicRobot extends Robot {
-
-	public OpMode opMode;
-	public HardwareMap hardwareMap;
 
 	public MecanumDrive mecanumDrive;
 	public CarouselSpinner spinner;
@@ -25,18 +20,20 @@ public class BasicRobot extends Robot {
 
 		super( op );
 
+		// set opMode and hardwareMap from super (Robot)
 		opMode = op;
-		hardwareMap = op.hardwareMap;
+		hardwareMap = opMode.hardwareMap;
 
-		// initialize util objects/classes
-//		tfod = new TensorFlowUtil( opMode );
-
+		// initialize subsystems and utilities
 		super.driveTrain = new MecanumDrive( hardwareMap );
 		mecanumDrive = (MecanumDrive) driveTrain;
+
 		spinner = new CarouselSpinner( hardwareMap );
 		lift = new Lift( hardwareMap );
 		bucket = new Bucket( hardwareMap );
 		intake = new Intake( hardwareMap );
+
+//		tfod = new TensorFlowUtil( opMode );
 
 	}
 
