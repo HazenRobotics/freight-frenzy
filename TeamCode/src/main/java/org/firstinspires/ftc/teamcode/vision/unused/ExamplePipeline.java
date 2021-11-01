cf4425470d1b5f5b79345bd28145a1f68ce94d77
hazen <sam.dep000@rentonstudent.us>
2021-11-01 16:02:01 -0700

    Implementing OpenCV Classes.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/unused/ExamplePipeline.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.vision.unused;
+
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+public class ExamplePipeline extends OpenCvPipeline  {
+
+	@Override
+	public Mat processFrame( Mat input ) {
+		Mat output = input;
+//		Imgproc.blur( input, output, new Size( 4, 4 ) );
+		return output;
+	}
+}
