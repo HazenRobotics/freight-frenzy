ec0c34a0681a8fe63b963d6eb45888a5019ab648
hazen <sam.dep000@rentonstudent.us>
2021-12-03 18:51:07 -0800

    got contours set up, need to finish calibration of cube vs weight vertex count

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/CubeWeightDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/CubeWeightDetector.java
@@ -1,11 +1,10 @@
 package org.firstinspires.ftc.teamcode.vision;
 
-import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
-
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
@@ -13,12 +12,8 @@ import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvPipeline;
 
-import java.security.SecureClassLoader;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 
 public class CubeWeightDetector extends OpenCvPipeline {
 
@@ -43,45 +38,54 @@ public class CubeWeightDetector extends OpenCvPipeline {
 
 	public Mat processFrame( Mat input, String type ) {
 
-		return processEdgeFrame( processHSVColorFrame( input ) );
+		return processHSVColorFrame( input );
 	}
 
 	public Mat processEdgeFrame( Mat src ) {
-//		Mat colorMask = new Mat(src.rows(), src.cols(), src.type());
-//		Mat gray = new Mat(src.rows(), src.cols(), src.type());
-//		Mat edges = new Mat(src.rows(), src.cols(), src.type());
-//		Mat dst = new Mat(src.rows(), src.cols(), src.type(), new Scalar(0));
 
-//		Imgproc.cvtColor( src, colorMask, Imgproc.COLOR_RGB2HSV ); //f
-//		Scalar lowHSV = new Scalar( 17, 0, 0 ); //f 17
-//		Scalar highHSV = new Scalar( 55, 255, 255 ); //f 23
-//		Core.inRange( colorMask, lowHSV, highHSV, colorMask);
-
-		//Gray image
-//		Imgproc.cvtColor( src, gray, Imgproc.COLOR_RGB2GRAY);
-		//Blur image from 1,1px to 5,5px
-//		Imgproc.blur(gray, edges, new Size(5,5));
-		//Detect edge
-		Imgproc.Canny(src, src, 25, 100);
-		Imgproc.morphologyEx( src,src,Imgproc.MORPH_DILATE, Imgproc.getStructuringElement( Imgproc.MORPH_RECT, new Size( 2, 2 ) ) );
+		//Canny Edge Filter
+		Imgproc.Canny( src, src, 25, 100 );
+		//Dilation Filter
+		Imgproc.morphologyEx( src, src, Imgproc.MORPH_DILATE, Imgproc.getStructuringElement( Imgproc.MORPH_RECT, new Size( 2, 2 ) ) );
 
 		//copy detected edges to destination matrix
-		List<MatOfPoint> points = new ArrayList<>();
-		Mat hierarchy = new Mat();
-		Imgproc.findContours( src, points, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE );
-		Imgproc.cvtColor(src, src, Imgproc.COLOR_GRAY2BGR);
-		Iterator<MatOfPoint> it = points.iterator();
-		while(it.hasNext()) {
-			telemetry.addLine(""+it.next());
+		//Create list of points and Mat
+		List<MatOfPoint> matsOfPoints = new ArrayList<>( );
+		Mat hierarchy = new Mat( );
+		//Find Contours
+		Imgproc.findContours( src, matsOfPoints, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE );
+		Imgproc.cvtColor( src, src, Imgproc.COLOR_GRAY2BGR );
+		List<MatOfPoint> contourList = new ArrayList<>( );
+		MatOfPoint2f approx = new MatOfPoint2f( );
+
+		//Convert MatOfPoints to Point[]
+
+		for( int i = 0; i < matsOfPoints.size( ); i++ ) {
+			Point[] points = matsOfPoints.get( i ).toArray( );
+			Imgproc.approxPolyDP( new MatOfPoint2f( points ), approx, 0.01 * Imgproc.arcLength( new MatOfPoint2f( points ), true ), true );
+			double area = Imgproc.contourArea( matsOfPoints.get( i ) );
+			if( (approx.toArray( ).length > 8) && (approx.toArray( ).length < 30) && (approx.toArray( ).length < 23) && (area > 30) )
+				contourList.add( new MatOfPoint( points ) );
 		}
-		telemetry.update();
+
+		if (contourList.size() >= 1) {
+			freightType = FreightType.WEIGHTED_CUBE;
+			telemetry.addLine( "Weighted Cube" );
+		} else {
+			freightType = FreightType.UNWEIGHTED_CUBE;
+			telemetry.addLine( "Unweighted Cube" );
+		}
+
+
+		Imgproc.drawContours( src, contourList, -1, new Scalar( 255, 0, 0 ), 2 );
+
 		return src;
 	}
 
 	public Mat processHSVColorFrame( Mat input ) {
 		Imgproc.cvtColor( input, mat, Imgproc.COLOR_RGB2HSV );//f
 		//Imgproc.blur(mat, mat, new Size(3,3));
-		Mat matWeight = mat.clone();
+//		Mat matWeight = mat.clone( );
 
 		Scalar freightLowHSV = new Scalar( 4, 0, 0 ); //f 17
 		Scalar freightHighHSV = new Scalar( 29, 255, 255 ); //f 23
@@ -89,61 +93,68 @@ public class CubeWeightDetector extends OpenCvPipeline {
 		Scalar weightHighHSV = new Scalar( 100, 30, 255 ); //w
 
 		SetupMatrix( mat, freightLowHSV, freightHighHSV );
-		Rect BUCKET = new Rect ( 54, 0, 212, 240 );
-		for( int i = 0; i < 10; i++ ) {
+		Rect BUCKET = new Rect( 54, 0, 212, 240 );
+		for( int i = 0; i < 100; i++ ) {
 			Imgproc.morphologyEx( mat, mat, Imgproc.MORPH_CLOSE, Imgproc.getStructuringElement( Imgproc.MORPH_RECT, new Size( 2, 2 ) ) );
 		}
-		Rect FREIGHT_ROI = ScanMinArea( mat, BUCKET); //f
+		Rect FREIGHT_ROI = ScanMinArea( mat, BUCKET ); //f
 		Mat freightArea = mat.submat( FREIGHT_ROI );//f
 
 
-		SetupMatrix( matWeight, weightLowHSV, weightHighHSV );
-		Imgproc.morphologyEx( matWeight, matWeight, Imgproc.MORPH_DILATE, Imgproc.getStructuringElement( Imgproc.MORPH_RECT, new Size(3,3)));
-		Rect WEIGHT_ROI = ScanMinArea( matWeight,FREIGHT_ROI );
-		Mat weightArea = matWeight.submat( WEIGHT_ROI );
+//		SetupMatrix( matWeight, weightLowHSV, weightHighHSV );
+//		Imgproc.morphologyEx( matWeight, matWeight, Imgproc.MORPH_DILATE, Imgproc.getStructuringElement( Imgproc.MORPH_RECT, new Size( 3, 3 ) ) );
+//		Rect WEIGHT_ROI = ScanMinArea( matWeight, FREIGHT_ROI );
+//		Mat weightArea = matWeight.submat( WEIGHT_ROI );
 
 		double freightValue = Core.sumElems( freightArea ).val[0] / FREIGHT_ROI.area( ) / 255; //f
-		double weightValue = Core.sumElems( weightArea ).val[0] / WEIGHT_ROI.area( ) / 255; //f
+//		double weightValue = Core.sumElems( weightArea ).val[0] / WEIGHT_ROI.area( ) / 255; //f
 
-		telemetry.addLine("FV" + freightValue);
-		telemetry.addLine("WV" + weightValue);
+		telemetry.addLine( "FV" + freightValue );
+//		telemetry.addLine( "WV" + weightValue );
 
-		telemetry.addLine("FROI X" + FREIGHT_ROI.x);
-		telemetry.addLine("FROI Y" + FREIGHT_ROI.y);
-		telemetry.addLine("WROI X" + WEIGHT_ROI.x);
-		telemetry.addLine("WROI Y" + WEIGHT_ROI.y);
+		telemetry.addLine( "FROI X" + FREIGHT_ROI.x );
+		telemetry.addLine( "FROI Y" + FREIGHT_ROI.y );
+//		telemetry.addLine( "WROI X" + WEIGHT_ROI.x );
+//		telemetry.addLine( "WROI Y" + WEIGHT_ROI.y );
 
 		freightArea.release( ); //f
-		weightArea.release();
+//		weightArea.release( );
 
 		boolean freightBool = freightValue > FREIGHT_THRESHOLD; //f
-		boolean weightBool = weightValue > WEIGHT_THRESHOLD;
+//		boolean weightBool = weightValue > WEIGHT_THRESHOLD;
 
 		Scalar red = new Scalar( 255, 0, 0 ); //f
 		Scalar green = new Scalar( 0, 255, 0 ); //f
-		Scalar blue = new Scalar( 0,0, 255);
-		Scalar yellow = new Scalar(255, 255, 0);
-		Scalar orange = new Scalar(255, 165, 0);
-		Scalar pink = new Scalar (255, 0, 255);
-		Scalar black = new Scalar( 0,0,0 );
+		Scalar blue = new Scalar( 0, 0, 255 );
+		Scalar yellow = new Scalar( 255, 255, 0 );
+		Scalar orange = new Scalar( 255, 165, 0 );
+		Scalar pink = new Scalar( 255, 0, 255 );
+		Scalar black = new Scalar( 0, 0, 0 );
 		Scalar id = blue;
-		if( freightBool & weightBool ) { //f
-			freightType = FreightType.WEIGHTED_CUBE;
-			telemetry.addLine( "Weighted Cube" );
-			id = yellow;
-		} else if( freightBool ) { //f
-			freightType = FreightType.UNWEIGHTED_CUBE;
-			telemetry.addLine( "Unweighted Cube" );
-			id = orange;
+
+		if (freightBool) {
+			return processEdgeFrame( mat );
 		} else {
 			freightType = FreightType.NOT_CUBE;
 			telemetry.addLine( " Not Cube" );
+			return mat;
 		}
 
+//		if( freightBool & weightBool ) { //f
+//			freightType = FreightType.WEIGHTED_CUBE;
+//			telemetry.addLine( "Weighted Cube" );
+//			id = yellow;
+//		} else if( freightBool ) { //f
+//			freightType = FreightType.UNWEIGHTED_CUBE;
+//			telemetry.addLine( "Unweighted Cube" );
+//			id = orange;
+//		} else {
+//			freightType = FreightType.NOT_CUBE;
+//			telemetry.addLine( " Not Cube" );
+//		}
 
 
 
-		Mat current = mat;
 
 //		Imgproc.cvtColor( current, current, Imgproc.COLOR_GRAY2RGB ); //i
 
@@ -153,7 +164,7 @@ public class CubeWeightDetector extends OpenCvPipeline {
 //		Rect BUCKETEDGE = new Rect ( 165, 0, 20 , 240 );
 //		Imgproc.rectangle( current, BUCKETEDGE, orange);
 
-		return current;
+
 	}
 
 	@Override
@@ -169,8 +180,8 @@ public class CubeWeightDetector extends OpenCvPipeline {
 		return freightType;
 	}
 
-	public Rect ScanMatrix (Mat mat) {
-		return ScanMatrix(mat, new Rect(0,0,320,240));
+	public Rect ScanMatrix( Mat mat ) {
+		return ScanMatrix( mat, new Rect( 0, 0, 320, 240 ) );
 		/*int minRow = mat.rows(), maxRow = 0, minCol = mat.cols(), maxCol = 0;
 
 		for(int row = 0; row < mat.rows(); row++){
@@ -198,12 +209,12 @@ public class CubeWeightDetector extends OpenCvPipeline {
 		return CUBE_ROI;*/
 	}
 
-	public Rect ScanMatrix (Mat mat, Rect scanArea) {
-		int minRow = mat.rows(), maxRow = 0, minCol = mat.cols(), maxCol = 0;
+	public Rect ScanMatrix( Mat mat, Rect scanArea ) {
+		int minRow = mat.rows( ), maxRow = 0, minCol = mat.cols( ), maxCol = 0;
 
-		for(int row = scanArea.y; row < scanArea.y + scanArea.height; row++){
-			for(int col = scanArea.x; col < scanArea.x + scanArea.width; col++){
-				if (mat.get(row,col) != null) {
+		for( int row = scanArea.y; row < scanArea.y + scanArea.height; row++ ) {
+			for( int col = scanArea.x; col < scanArea.x + scanArea.width; col++ ) {
+				if( mat.get( row, col ) != null ) {
 					double point = mat.get( row, col )[0];
 					if( point == 255 & row < minRow ) {
 						minRow = row;
@@ -227,38 +238,36 @@ public class CubeWeightDetector extends OpenCvPipeline {
 		return CUBE_ROI;
 	}
 
-	public Rect ScanMinArea (Mat mat, Rect scanArea) {
-		int minRow = 0, maxRow = mat.rows(), minCol = 0, maxCol = mat.cols();
+	public Rect ScanMinArea( Mat mat, Rect scanArea ) {
+		int minRow = 0, maxRow = mat.rows( ), minCol = 0, maxCol = mat.cols( );
 		int maxRowCt = 0, maxColCt = 0, curRowCt = 0, curColCt = 0;
-		for(int row = scanArea.y; row < scanArea.y + scanArea.height; row++){
-			for(int col = scanArea.x; col < scanArea.x + scanArea.width; col++){
-				if (mat.get(row,col) != null) {
+		for( int row = scanArea.y; row < scanArea.y + scanArea.height; row++ ) {
+			for( int col = scanArea.x; col < scanArea.x + scanArea.width; col++ ) {
+				if( mat.get( row, col ) != null ) {
 					double point = mat.get( row, col )[0];
-					if( point == 255) {
+					if( point == 255 ) {
 						curColCt++;
-						if( maxColCt < curColCt) {
+						if( maxColCt < curColCt ) {
 							maxColCt = curColCt;
 							maxCol = col;
 						}
-					}
-					else {
+					} else {
 						curColCt = 0;
 					}
 				}
 			}
 		}
-		for(int col = scanArea.x; col < scanArea.x + scanArea.width; col++) {
-			for(int row = scanArea.y; row < scanArea.y + scanArea.height; row++) {
-				if (mat.get(row,col) != null) {
+		for( int col = scanArea.x; col < scanArea.x + scanArea.width; col++ ) {
+			for( int row = scanArea.y; row < scanArea.y + scanArea.height; row++ ) {
+				if( mat.get( row, col ) != null ) {
 					double point = mat.get( row, col )[0];
-					if( point == 255) {
+					if( point == 255 ) {
 						curRowCt++;
 						if( maxRowCt < curRowCt ) {
 							maxRowCt = curRowCt;
 							maxRow = row;
 						}
-					}
-					else {
+					} else {
 						curRowCt = 0;
 					}
 				}
@@ -269,16 +278,16 @@ public class CubeWeightDetector extends OpenCvPipeline {
 //		telemetry.addLine("("+minRow+","+minCol+") ");
 //		telemetry.addLine("("+maxRow+","+maxCol+") ");
 		minRow = minRow < scanArea.y ? scanArea.y : minRow;
-		maxRow = maxRow > mat.rows() ? mat.rows() : maxRow;
+		maxRow = maxRow > mat.rows( ) ? mat.rows( ) : maxRow;
 		minCol = minCol < scanArea.x ? scanArea.x : minCol;
-		maxCol = maxCol > mat.cols() ? mat.cols() : maxCol;
+		maxCol = maxCol > mat.cols( ) ? mat.cols( ) : maxCol;
 		Rect CUBE_ROI = new Rect(
 				new Point( minCol, minRow ),
 				new Point( maxCol, maxRow ) );
 		return CUBE_ROI;
 	}
 
-	public void SetupMatrix(Mat mat, Scalar lowHSV, Scalar highHSV) {
+	public void SetupMatrix( Mat mat, Scalar lowHSV, Scalar highHSV ) {
 		Core.inRange( mat, lowHSV, highHSV, mat );
 	}
 }
