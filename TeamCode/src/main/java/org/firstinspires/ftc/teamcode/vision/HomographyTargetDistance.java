5083f9e82f802d487d0cb94722e9747ac133dc99
hazen <sam.dep000@rentonstudent.us>
2022-01-12 16:39:34 -0800

    changed values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/HomographyTargetDistance.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/vision/HomographyTargetDistance.java
@@ -13,21 +13,21 @@ public class HomographyTargetDistance {
 
 	//Value from solvePnP
 	private static final Matrix TRANSLATION_VECTOR = new Matrix( new double[][] {
-			{-38.29131528},
-			{113.80422094},
-			{659.27536728}
+			{78.08870033},
+			{110.767108},
+			{397.27562884}
 	} );
 	//Value after applying Rodriguez rotation formula to rotation vector from solvePnP
 	private static final Matrix ROTATION_MATRIX = new Matrix( new double[][] {
-			{-0.99985521, -0.01629314,  0.00490925},
-			{0.00818302, -0.20741969,  0.97821782},
-			{-0.01491996,  0.97811636,  0.20752298}
+			{-9.99849474e-01, -1.73249749e-02,  9.34937086e-04},
+			{6.96330302e-03, -3.51339381e-01,  9.36222277e-01},
+			{-1.58915472e-02,  9.36087862e-01,  3.51407134e-0}
 	} );
 	//Value from camera calibration
 	private static final Matrix CAMERA_MATRIX = new Matrix( new double[][] {
-			{1.43380471e+03, 0.00000000e+00, 6.88915495e+02},
-			{0.00000000e+00, 1.43982105e+03, 3.28228935e+02},
-			{0.00000000e+00, 0.00000000e+00, 1.00000000e+00}
+			{823.41290477,   0.0,         359.87721122},
+			{0.0,         825.66798289, 210.74873307},
+			{0.0, 0.0, 1.0}
 	} );
 	//Z axis is always 1 since all the objects are on the ground
 	final static double Z_CONST = 1;
