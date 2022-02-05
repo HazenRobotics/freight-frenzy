729fbf85977ab75f1f1435d58073423b6971f62b
camdencambre <camdenharris@gmail.com>
2022-02-04 19:50:59 -0800

    Integrated bucket object detection into code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/BasicTeleOp.java
@@ -0,0 +1,76 @@
+package org.firstinspires.ftc.teamcode.teleops;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+// 13" robot with laptop on it
+@TeleOp(name = "BasicTeleOp", group = "TeleOp")
+public  class BasicTeleOp extends OpMode {
+
+	public static String CLASS_NAME = "AutoDemonstration";
+
+	DcMotor frontLeft;
+	DcMotor frontRight;
+	DcMotor backLeft;
+	DcMotor backRight;
+
+	@Override
+	public void init() {
+
+		frontLeft = hardwareMap.dcMotor.get("frontLeft");
+		frontRight = hardwareMap.dcMotor.get("frontRight");
+		backLeft = hardwareMap.dcMotor.get("backLeft");
+		backRight = hardwareMap.dcMotor.get("backRight");
+
+		frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+		backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+		addAndUpdate(CLASS_NAME + " : finished init");
+
+
+	}
+
+
+
+	@Override
+	public void loop() {
+
+		double drive =  -gamepad1.left_stick_y;
+		double strafe = gamepad1.left_stick_x;
+		double rotate = gamepad1.right_stick_x;
+
+		double frontLeftPower = drive + strafe + rotate;
+		double frontRightPower = drive - strafe - rotate;
+		double backLeftPower = drive - strafe + rotate;
+		double backRightPower = drive + strafe - rotate;
+
+		setPowers( frontLeftPower, frontRightPower, backLeftPower, backRightPower );
+
+
+		telemetry.update();
+	}
+
+	public void addAndUpdate( String text ) {
+		telemetry.addLine( text );
+		telemetry.update();
+
+	}
+
+	public void setPowers(double frontLeftPower, double frontRightPower, double backLeftPower, double backRightPower) {
+
+		frontLeft.setPower(frontLeftPower);
+		frontRight.setPower(frontRightPower);
+		backLeft.setPower(backLeftPower);
+		backRight.setPower(backRightPower);
+
+		telemetry.addLine("FL: " + frontLeftPower + ", FR: " + frontRightPower);
+		telemetry.addLine("BL: " + backLeftPower + ", BR: " + backRightPower);
+
+	}
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/DetectorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/DetectorTest.java
@@ -31,6 +31,7 @@ package org.firstinspires.ftc.teamcode.tests;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.tfrec.Detector;
@@ -38,31 +39,58 @@ import org.firstinspires.ftc.teamcode.tfrec.classification.Classifier;
 
 import java.util.List;
 
+import javax.annotation.Nullable;
+
 //Opmode for quick testing of motors
 @TeleOp(name="TFDetector", group="Robot15173")
 
 public class DetectorTest extends LinearOpMode{
 
     // Declare OpMode members.
-    private Detector tfDetector = null;
-    private ElapsedTime runtime = new ElapsedTime();
+    private Detector tfDetector;
+    private ElapsedTime runtime;
 
-    private static String MODEL_FILE_NAME = "loaded_vs_unloaded.tflite";
-    private static String LABEL_FILE_NAME = "loaded_vs_unloaded_labels.txt";
-    private static Classifier.Model MODEl_TYPE = Classifier.Model.FLOAT_EFFICIENTNET;
+    private static String MODEL_FILE_NAME;
+    private static String LABEL_FILE_NAME;
+    private static Classifier.Model MODEl_TYPE;
+
+
+    public void initializeClassificationDetector( String tfliteFile, String labelFile, HardwareMap hm ) {
+        tfDetector = null;
+        runtime = new ElapsedTime();
+        MODEL_FILE_NAME = tfliteFile;
+        LABEL_FILE_NAME = labelFile;
+        MODEl_TYPE = Classifier.Model.FLOAT_EFFICIENTNET;
+        try {
+            tfDetector = new Detector(MODEl_TYPE, MODEL_FILE_NAME, LABEL_FILE_NAME, hm.appContext, telemetry);
+            tfDetector.activate();
+        }
+        catch (Exception ex){
+            telemetry.addData("Error", String.format("Unable to initialize Detector. %s", ex.getMessage()));
+            sleep(3000);
+            return;
+        }
+    }
+
+    @Nullable
+    public List<Classifier.Recognition> getResults() {
+        List<Classifier.Recognition> results = tfDetector.getLastResults();
+        if (results == null || results.size() == 0){
+            telemetry.addData("Info", "No results");
+        }
+        else {
+            for (Classifier.Recognition r : results) {
+                String item = String.format("%s: %.2f", r.getTitle(), r.getConfidence());
+                telemetry.addData("Found", item);
+            }
+        }
+        return results;
+    }
 
     @Override
     public void runOpMode() {
         try {
-            try {
-                tfDetector = new Detector(MODEl_TYPE, MODEL_FILE_NAME, LABEL_FILE_NAME, hardwareMap.appContext, telemetry);
-                tfDetector.activate();
-            }
-            catch (Exception ex){
-                telemetry.addData("Error", String.format("Unable to initialize Detector. %s", ex.getMessage()));
-                sleep(3000);
-                return;
-            }
+            initializeClassificationDetector( "loaded_vs_unloaded.tflite", "loaded_vs_unloaded.tflite", hardwareMap );
 
             telemetry.addData("Detector", "Ready");
             telemetry.update();
